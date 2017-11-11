c66794e7a90709398a04d8dffcf9b811fa24dbdd
mw <mawi0949@clorado.edu>
2017-11-11 12:03:50 -0700

    Added py file, and put hte html code into the py file

---
+++ PP_Wilmes_Bascom_HW4.py
@@ -0,0 +1,12 @@
+#! /usr/local/bin/python2
+print "Contest-type: text/html"
+contents = '''
+<html>
+	<head>
+	<title>HW #4 Wilmes Bascom</title> </head>
+<body>
+MapOfUnitedState.svg
+</body>
+</html>
+'''
+print contents
