a6e6fb5ceec45fad94b38ccd5e8d520bbdf33428
mw <mawi0949@clorado.edu>
2017-11-12 16:02:05 -0700

    Done #1, got up to #4 on part 2

--- PP_Wilmes_Bascom_HW4.py
+++ PP_Wilmes_Bascom_HW4.py
@@ -1,9 +1,24 @@
 #! /usr/local/bin/python2
+import requests
+import json
+
+response = requests.get('https://WEATHERSERVICEANDKEYHERE.com')
+data = json.load(response)
+print data
 print "Contest-type: text/html"
 contents = '''
 <html>
 	<head>
-	<title>HW #4 Wilmes Bascom</title> </head>
+	<title>HW #4 Wilmes Bascom</title>
+	<script src="http://code.jquery.com/jquery-2.2.1.js"></script>
+	<script>
+	$(document).ready(function(){
+		print "$('#CO').css('fill','red')"
+	'''
+	print '''
+	});
+
+	</script> </head>
 <body>
 MapOfUnitedState.svg
 </body>
